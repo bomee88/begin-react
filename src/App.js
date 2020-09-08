e1f813c22775a71bd76bc3270dbefe7c685ef609
yubomi <bomee88@naver.com>
2020-09-08 17:22:53 +0900

    new code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
+import produce from 'immer';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
@@ -34,24 +34,35 @@ const initialState = {
 function reducer(state, action){
   switch (action.type){
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      })
+      // return {
+      //   inputs: initialState.inputs,
+      //   users: state.users.concat(action.user)
+      // }
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id
-          ? { ...user, active: !user.active }
-          : user
-          )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.map(user =>
+      //     user.id === action.id
+      //     ? { ...user, active: !user.active }
+      //     : user
+      //     )
+      // };
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => { //splice로 지워줌
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+      // return {
+      //   ...state,
+      //   users: state.users.filter(user => user.id !== action.id)
+      // }
     default:
       throw new Error('Unhandled action');
   }
--- src/Hello.js
+++ src/Hello.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function Hello({color, name, isSpecial}){ 
-const style = {
-    color: color,
-    paddingBottom: 20
-}
-return (
-    <>
-        <div>안녕하세요 {name}입니다.</div>  
-        <div style={style}>내가 좋아하는 컬러는 {color}입니다.
-            {isSpecial && <div>진짜예요!</div> || <div>농담이에요</div>}
-        </div>
-    </>
-    ) 
-}
-Hello.defaultProps = {
+class Hello extends Component {
+  static defaultProps = { //class에서는 defaultProps를 이렇게 설정해줄 수도 있음.
     name: '이름없음',
-    color: 'blue'
+    color: ''
+  }
+  render(){
+    const { color, isSpecial, name } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    )
+  }
 }
+
+
+// function Hello({color, name, isSpecial}){ 
+// const style = {
+//     color: color,
+//     paddingBottom: 20
+// }
+// return (
+//     <>
+//         <div>안녕하세요 {name}입니다.</div>  
+//         <div style={style}>내가 좋아하는 컬러는 {color}입니다.
+//             {isSpecial && <div>진짜예요!</div> || <div>농담이에요</div>}
+//         </div>
+//     </>
+//     ) 
+// }
+// Hello.defaultProps = {
+//     name: '이름없음',
+//     color: 'blue'
+// }
 
 export default Hello; 
